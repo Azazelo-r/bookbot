2e54d0658279e8ab5ea8192fa6c7f52be950490b
Azazelo-r <yanbekovd@bk.ru>
2024-07-04 03:29:44 +0500

    small formatting changes

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 def main():
-    path = 'books/frankenstein.txt'
+    path = "books/frankenstein.txt"
     print(f"--- Begin report of {path} ---")
     print(f"{get_total_words(get_book_text(path))} words found in the document\n")
-    for k, v in sorted(get_total_characters(get_book_text(path)).items(), key=lambda x: -x[1]):
+    for k, v in sorted(
+        get_total_characters(get_book_text(path)).items(), key=lambda x: -x[1]
+    ):
         print(f"The {k!r} character was found {v} times")
     print("--- End report ---")
-
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
+
+
 def get_total_words(text):
     return len(text.split())
-    
+
+
 def get_total_characters(text):
     res = {}
     for c in text.lower():
@@ -22,5 +25,5 @@ def get_total_characters(text):
             res[c] = res.get(c, 0) + 1
     return res
 
-    
+
 main()
